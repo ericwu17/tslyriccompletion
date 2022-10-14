34bfe3eccac7d4d05d68177a2baa67987f59e741
Eric Wu <eric.dianhao.wu@gmail.com>
2022-10-14 14:40:48 -0700

    some more game flow functions

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ mod diff;
 
 use std::error::Error;
 use song::Song;
-use game::run_game_loop;
-
+use game::{run_game_loop, input};
+use diff::diff_greedy;
 
 fn main() -> Result<(), Box<dyn Error>> {
-	// let d = diff_greedy("helo", "hello");
-	// println!("{:?}", d);
-
+	
+	println!("{:?}", diff_greedy(&"Now all we know is don’t let go".to_lowercase(), &"You showed me colors you know I can't see with anyone else".to_lowercase()));
+	input("");
 	run_game_loop();
 
 	Ok(())
