4e13fc0ddfef4f9aae81316a846fb0f222fa7a51
Eric Wu <eric.dianhao.wu@gmail.com>
2022-11-06 14:24:33 -0800

    change baseURL of axios

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://linux.ucla.edu/tswift/api/';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
