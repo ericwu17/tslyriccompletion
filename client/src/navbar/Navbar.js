edcbb06efb30e6a3b442f03f173dffe559e2f199
Eric Wu <eric.dianhao.wu@gmail.com>
2022-11-06 22:51:10 -0800

    fix navbar hiding page issue
    The previous solution was to use a margin of 10 (hard coded)
    This was bad because on mobile views the navbar was > 10 MUI units
    in height, and so the navbar would partially obscure the page

--- client/src/navbar/Navbar.js
+++ client/src/navbar/Navbar.js
@@ -7,43 +7,54 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import HomeIcon from '@mui/icons-material/Home';
 export default function Navbar() {
+  const toolbar = (
+    <Toolbar>
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+        onClick={() => {window.location.href="/tswift"}}
+      >
+        <HomeIcon />
+      </IconButton>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        Taylor Swift Lyric Guessing Game
+      </Typography>
+      <Button 
+        color="inherit"
+        onClick={() => {window.location.href="/tswift/play"}}
+      >
+        Start Game
+      </Button>
+      <Button 
+        color="inherit"
+        onClick={() => {window.location.href="/tswift/history"}}
+      >
+        View Scores
+      </Button>
+      <Button 
+        color="inherit"
+        onClick={() => {window.location.href="/tswift/song"}}
+      >
+        View Lyrics
+      </Button>
+    </Toolbar>
+  );
+
   return (
-    <Box sx={{ flexGrow: 1 }} mb={10}>
+    <Box>
       <AppBar position="fixed">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={() => {window.location.href="/tswift"}}
-          >
-            <HomeIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Taylor Swift Lyric Guessing Game
-          </Typography>
-          <Button 
-            color="inherit"
-            onClick={() => {window.location.href="/tswift/play"}}
-          >
-            Start Game
-          </Button>
-          <Button 
-            color="inherit"
-            onClick={() => {window.location.href="/tswift/history"}}
-          >
-            View Scores
-          </Button>
-          <Button 
-            color="inherit"
-            onClick={() => {window.location.href="/tswift/song"}}
-          >
-            View Lyrics
-          </Button>
-        </Toolbar>
+        {toolbar}
       </AppBar>
+
+      {/* We render the toolbar twice: once in the AppBar and once in the page,
+       so that the content of the page is not hidden beneath the AppBar.
+       */}
+       <Box visibility="hidden">
+        {toolbar}
+       </Box>
     </Box>
   );
 }
