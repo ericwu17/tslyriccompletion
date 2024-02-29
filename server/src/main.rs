58f9d884382613425ecdb66a880da26c4c145b46
Eric <ericwu17@protonmail.com>
2024-02-28 17:29:51 -0800

    add new lyrics data

--- server/src/main.rs
+++ server/src/main.rs
@@ -39,37 +39,36 @@ async fn main() -> Result<(), rocket::Error> {
     let my_hashmap: HashMap<String, GameState> = HashMap::new();
     let game_state = Arc::new(Mutex::new(my_hashmap));
 
-    let database_url = format!("mysql://{}:{}@localhost:3306/mydb", db_user, db_pw);
-    println!("Connecting to MySql Database...");
-    let pool = MySqlPoolOptions::new()
-        .max_connections(5)
-        .connect(&database_url)
-        .await
-        .expect("Failed to connect to database");
-    println!("Connection established!");
+    // let database_url = format!("mysql://{}:{}@localhost:3306/mydb", db_user, db_pw);
+    // println!("Connecting to MySql Database...");
+    // let pool = MySqlPoolOptions::new()
+    //     .max_connections(5)
+    //     .connect(&database_url)
+    //     .await
+    //     .expect("Failed to connect to database");
+    // println!("Connection established!");
 
-    let rocket = rocket::build()
-        .manage(game_state)
-        .manage(songs)
-        .manage(pool)
-        .mount("/", routes![index])
-        .mount("/", routes![get_song_list])
-        .mount("/", routes![get_song_list_with_id])
-        .mount("/", routes![get_all_songlists])
-        .mount("/", routes![get_song])
-        .mount("/", routes![init_game])
-        .mount("/", routes![game_lifelines])
-        .mount("/", routes![reduce_multiple_choice])
-        .mount("/", routes![next_question])
-        .mount("/", routes![claim_game])
-        .mount("/", routes![take_guess])
-        .mount("/", routes![get_games])
-        .mount("/", routes![get_game])
-        .mount("/", routes![get_line])
-        .ignite()
-        .await?;
+    // let rocket = rocket::build()
+    //     .manage(game_state)
+    //     .manage(songs)
+    //     .mount("/", routes![index])
+    //     .mount("/", routes![get_song_list])
+    //     .mount("/", routes![get_song_list_with_id])
+    //     .mount("/", routes![get_all_songlists])
+    //     .mount("/", routes![get_song])
+    //     .mount("/", routes![init_game])
+    //     .mount("/", routes![game_lifelines])
+    //     .mount("/", routes![reduce_multiple_choice])
+    //     .mount("/", routes![next_question])
+    //     .mount("/", routes![claim_game])
+    //     .mount("/", routes![take_guess])
+    //     .mount("/", routes![get_games])
+    //     .mount("/", routes![get_game])
+    //     .mount("/", routes![get_line])
+    //     .ignite()
+    //     .await?;
 
-    let _ = rocket.launch().await?;
+    // let _ = rocket.launch().await?;
 
     Ok(())
 }
