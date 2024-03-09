2ca40557f62eb407555a0cfb8b6f0c743dc743ee
Eric <ericwu17@protonmail.com>
2024-03-09 23:22:05 +0000

    update build script

--- server/run_build.sh
+++ server/run_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cargo build --release
+cargo install --path .
 sudo systemctl restart tswiftrs
 
 echo "Successfully built the project and restarted the tswiftrs service. Changes should now be live."
